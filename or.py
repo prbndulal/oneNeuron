b11bfff3fe1655b2a36822508051227c381bf1d4
prabindulal <prbndulal@gmail.com>
2021-09-18 22:58:56 +1000

    implemented the logging and added tqdm

--- or.py
+++ or.py
@@ -2,27 +2,46 @@ from utils.model import Perceptron
 from utils.all_utils import prepare_data, save_plot, save_model
 import pandas as pd
 import numpy as np
+import logging
+import os
+
+logging_str="[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_dir="logs"
+os.makedirs(log_dir,exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log"),level=logging.INFO,format=logging_str)
+
+def main(data, modelName, plotName, eta, epochs):
+    df = pd.DataFrame(OR)
+
+    print(df)
+
+    X,y = prepare_data(df)
+
+ 
+
+    model = Perceptron(eta=ETA, epochs=EPOCHS)
+    model.fit(X, y)
+
+    _ = model.total_loss()
+
+    save_model(model, filename="or.model")
+    save_plot(df, "or.png", model)
+if __name__ == "__main__":
+
+    OR = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,1,1,1],
+    }
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 1000
+    try:
+        logging.info(">>> starting the training >>>>")
+        main(data=OR, modelName="or.model", plotName="or.png", eta=ETA, epochs=EPOCHS)
+        logging.info("<<< trainif done successfully<<<<")
+    except Exception as e:
+        logging.exception(e)
+        raise e
 
 
-OR = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,1,1,1],
-}
-
-df = pd.DataFrame(OR)
-
-print(df)
-
-X,y = prepare_data(df)
-
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
-
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
-
-_ = model.total_loss()
-
-save_model(model, filename="or.model")
-save_plot(df, "or.png", model)
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 import joblib # FOR SAVING MY MODEL AS A BINARY FILE
 from matplotlib.colors import ListedColormap
 import os
+import logging
 
 plt.style.use("fivethirtyeight")
 
@@ -16,6 +17,7 @@ def prepare_data(df):
   Returns:
      tuple: it returns the tuples of dependent variables and independent variables
   """
+  logging.info("preparing the data by segregating the dependent and independent variables")
   X = df.drop("y", axis=1)
   y = df["y"]
   return X, y
@@ -27,14 +29,17 @@ def save_model(model, filename):
       model (python object): trained model to 
       filename (str): path to save the trained model
   """
+  logging.info("saving the trained model")
   model_dir = "models"
   os.makedirs(model_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
   filePath = os.path.join(model_dir, filename) # model/filename
   joblib.dump(model, filePath)
+  logging.info(f"saved the trained model{filePath}")
 
 def save_plot(df, file_name, model):
 
   def _create_base_plot(df):
+    logging.info("creating the base plot")
     df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter")
     plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
     plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
@@ -42,6 +47,7 @@ def save_plot(df, file_name, model):
     figure.set_size_inches(10, 8)
 
   def _plot_decision_regions(X, y, classfier, resolution=0.02):
+    logging.info("plotting the decision regions")
     colors = ("red", "blue", "lightgreen", "gray", "cyan")
     cmap = ListedColormap(colors[: len(np.unique(y))])
 
@@ -74,3 +80,4 @@ def save_plot(df, file_name, model):
   os.makedirs(plot_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
   plotPath = os.path.join(plot_dir, file_name) # model/filename
   plt.savefig(plotPath)
+  logging.info(f"saving the plot at {plotPath}")
